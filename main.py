847ff1a793577d3d7daec0e5c14a05d412626dd2
warmap <basedfedorauser@gmail.com>
2024-08-30 14:52:17 +0300

    мне лень писать лог изменений, кому надо можете сами посмотреть

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if os.path.isfile("token.tok"):
 else:
     with open("token.tok", "w") as f:
         f.write("\n")
+        print('Пожалуйста, зайдите в Ваш аккаунт или зарегистрируйте новый если у вас его нет перед использованием.')
 
 # VARIABLES
 get = Get()
@@ -24,11 +25,6 @@ show_id_file = show_id
 
 def main():
     while True:
-        # if platform == 'linux':
-        #     subprocess.run('clear', shell=True)
-        # elif platform == 'win32' or 'win' in platform:
-        #     subprocess.run('cls', shell=True)
-
         token = get.token()
         menu(1, show_id)
         choice = input("Выбери действие: ")
@@ -46,10 +42,8 @@ def main():
                 if password != ">exit":
                     resp = requests.get(f"{host}/register?username={name}&password={password}")
                     errors(2, resp)
-                else:
-                    pass
-            else:
-                pass
+                else: pass
+            else: pass
 
         elif choice == "3":  # ПРОЧИТАТЬ СООБЩЕНИЯ
             resp = requests.get(f"{host}/read_messages?token={token}")
@@ -60,8 +54,7 @@ def main():
             if message != ">exit":
                 resp = requests.get(f"{host}/add_message?message={message}&token={token}")
                 errors(4, resp)
-            else:
-                pass
+            else: pass
 
         elif choice == "5":
             req = reqests.get(f"{host}/admin?token={token}&allow=False")
@@ -69,28 +62,22 @@ def main():
 
         elif choice == "6":
             while True:
-                # if platform == 'linux': subprocess.run('clear')
-                # elif platform == 'win32' or 'win' in platform: subprocess.run('cls')
-                menu(2)
+                menu(2, show_id)
                 setting = input("Введите новое значение по примеру выше: ")
                 setting = setting.split()
-                print(setting)
                 if setting[0] == "1":
                     if setting[1] == "True" or "False":
                         show_id == setting[1]
                         with open("data.json", "w") as f:
-                            # json.dump('', f, indent=4)
                             f.write("\n")
                             data = {"OS": platform, "show-id": show_id}
                             json.dump(data, f, indent=4)
-                    else:
-                        print("Неверное значение!")
-                if setting[0] == "0":
-                    break
-        elif choice == "0":
-            break
-        else:
-            print("Неверный выбор, попробуй снова.")
+
+                    else: print("Неверное значение!")
+
+                if setting[0] == "0": break
+        elif choice == "0": break
+        else: print("Неверный выбор, попробуй снова.")
 
 if __name__ == "__main__":
     main()
